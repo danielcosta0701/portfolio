95dff46a583d24f84e6076ab73be98cb744361f9
danielcosta0701 <contatodanielskt@gmail.com>
2025-02-21 01:24:36 -0300

    feat: add HTML structure and images

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,155 @@
+"use client"
+
+import { useRef } from "react";
+import { motion, useScroll, useTransform } from "framer-motion";
+import images from "@/public/images";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  // Animação seção
+  const targetRef = useRef(null);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+  const { scrollYProgress } = useScroll({
+    target: targetRef,
+    offset: ["start start", "end start"],
+  });
+
+  const backgroundColor = useTransform(scrollYProgress, [0, 1], ["#F8F8F8", "#EDEDED"]);
+
+  const experiences = [
+    {
+      title: "Front-end Developer",
+      date: "Jan 2023 - Present",
+      description: "Development of responsive and high-performance web interfaces."
+    },
+    {
+      title: "Front-end Developer",
+      date: "Jan 2023 - Present",
+      description: "Development of responsive and high-performance web interfaces."
+    }
+  ];
+
+  const contacts = [
+    {
+      label: "Email",
+      value: "contato@email.com",
+      type: "email",
+    },
+    {
+      label: "Telefone",
+      value: "+55 11 99999-9999",
+      type: "tel",
+    },
+    {
+      label: "LinkedIn",
+      value: "linkedin.com/in/seuperfil",
+      type: "url",
+      url: "https://linkedin.com/in/seuperfil",
+    },
+  ];
+
+  return (
+    <>
+      <main className="bg-off-white">
+        <section className="min-h-dvh flex">
+          <div className="flex-1 grid grid-cols-2 grid-rows-[1fr] gap-4 px-4 py-8 md:grid-cols-12">
+            <div className="col-span-full row-span-1 bg-dark-gray">
+              { /* image */}
+            </div>
+
+            <div className="col-span-full md:col-span-6">
+              <h1 className="scroll-m-20 text-6xl font-extrabold tracking-tight lg:text-5xl">
+                Haaron Santana
+              </h1>
+            </div>
+
+            <div className="col-span-full md:col-span-3">
+              <p className="leading-7 text-pretty font-bold text-2xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, sed?</p>
+            </div>
+          </div>
+        </section>
+
+        <motion.div
+          ref={targetRef}
+          className="overflow-y-scroll"
+          style={{ backgroundColor }}
+        >
+          <div className="grid grid-cols-2 gap-4 px-4 py-12 md:grid-cols-12">
+            {images.map((image, index) => (
+              <div key={index} className="w-full col-span-full">
+                <Image
+                  src={image.src}
+                  alt={image.alt}
+                  width={100}
+                  height={100}
+                  className="w-full"
+                />
+              </div>
+            ))}
+          </div>
+        </motion.div>
+
+        <section>
+          <div className="grid grid-cols-2 gap-4 px-4 py-12 md:grid-cols-12">
+            <div className="col-span-full md:col-span-6">
+              <h2 className="mt-10 scroll-m-20 text-6xl font-semibold tracking-tight transition-colors first:mt-0 md:sticky md:top-4">
+                Sobre
+              </h2>
+            </div>
+
+            <div className="flex flex-col gap-6 col-span-full md:col-span-6">
+              <p className="leading-7 font-semibold">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, sunt.
+              </p>
+
+              <p className="leading-7">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus nobis ab voluptatem. Fuga omnis ullam voluptatum ipsa harum blanditiis voluptas?
+              </p>
+
+              <div className="bg-dark-gray h-[400px]">
+                { /* image */}
+              </div>
+
+              <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+                Experiência
+              </h3>
+
+              <dl className="[&>div]:mt-6 [&>div:first-child]:mt-0">
+                {experiences.map((experience, index) => (
+                  <div key={index}>
+                    <div className="space-y-1">
+                      <dt className="font-semibold text-base">{experience.title}</dt>
+                      <dd className="text-gray-500">{experience.date}</dd>
+                    </div>
+                    <p className="leading-7 mt-2">{experience.description}</p>
+                  </div>
+                ))}
+              </dl>
+            </div>
+          </div>
+        </section>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+
+      <footer className="bg-dark-gray text-off-white">
+        <div className="grid grid-cols-2 gap-4 px-4 py-12 md:grid-cols-12">
+          <div className="col-span-full md:col-span-6 flex flex-col gap-12">
+            <h2 className="mt-10 scroll-m-20 text-6xl font-semibold text-off-white tracking-tight transition-colors first:mt-0">
+              Contato
+            </h2>
+
+            <dl className="[&>div]:mt-4 [&>div:first-child]:mt-0">
+              {contacts.map((contact, index) => (
+                <div key={index} className="space-y-1">
+                  <dt className="font-semibold text-[#6A6B71]">{contact.label}</dt>
+                  <dd className="text-off-white">
+                    {contact.value}
+                  </dd>
+                </div>
+              ))}
+            </dl>
+          </div>
+        </div>
       </footer>
-    </div>
+    </>
   );
 }
