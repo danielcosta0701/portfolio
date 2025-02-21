222deb007e9505fae19d20710f6347b2b450ce32
danieloliveira-ip4y <daniel.oliveira@ip4y.com.br>
2025-02-21 13:44:39 -0300

    feat: add  images adjusments

--- app/page.tsx
+++ app/page.tsx
@@ -1,53 +1,10 @@
-"use client"
-
-import { useRef } from "react";
-import { motion, useScroll, useTransform } from "framer-motion";
-import images from "@/public/images";
+import images from "../../data/images";
+import experiences from "../../data/experiences";
+import contacts from "../../data/contacts";
 import Image from "next/image";
 
 export default function Home() {
-  // Animação seção
-  const targetRef = useRef(null);
-
-  const { scrollYProgress } = useScroll({
-    target: targetRef,
-    offset: ["start start", "end start"],
-  });
-
-  const backgroundColor = useTransform(scrollYProgress, [0, 1], ["#F8F8F8", "#EDEDED"]);
-
-  const experiences = [
-    {
-      title: "Front-end Developer",
-      date: "Jan 2023 - Present",
-      description: "Development of responsive and high-performance web interfaces."
-    },
-    {
-      title: "Front-end Developer",
-      date: "Jan 2023 - Present",
-      description: "Development of responsive and high-performance web interfaces."
-    }
-  ];
-
-  const contacts = [
-    {
-      label: "Email",
-      value: "contato@email.com",
-      type: "email",
-    },
-    {
-      label: "Telefone",
-      value: "+55 11 99999-9999",
-      type: "tel",
-    },
-    {
-      label: "LinkedIn",
-      value: "linkedin.com/in/seuperfil",
-      type: "url",
-      url: "https://linkedin.com/in/seuperfil",
-    },
-  ];
-
+  const isEven = (number: number): boolean => number % 2 === 0
   return (
     <>
       <main className="bg-off-white">
@@ -58,36 +15,32 @@ export default function Home() {
             </div>
 
             <div className="col-span-full md:col-span-6">
-              <h1 className="scroll-m-20 text-6xl font-extrabold tracking-tight lg:text-5xl">
+              <h1 className="scroll-m-20 text-6xl font-bold tracking-tight lg:text-5xl">
                 Haaron Santana
               </h1>
             </div>
 
             <div className="col-span-full md:col-span-3">
-              <p className="leading-7 text-pretty font-bold text-2xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, sed?</p>
+              <p className="leading- font-semilbold text-2xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, sed?</p>
             </div>
           </div>
         </section>
 
-        <motion.div
-          ref={targetRef}
-          className="overflow-y-scroll"
-          style={{ backgroundColor }}
-        >
+        <div>
           <div className="grid grid-cols-2 gap-4 px-4 py-12 md:grid-cols-12">
             {images.map((image, index) => (
-              <div key={index} className="w-full col-span-full">
+              <div key={index} className={`w-full col-span-full flex ${isEven(index) ? "md:justify-start" : "md:justify-end"}`}>
                 <Image
                   src={image.src}
                   alt={image.alt}
-                  width={100}
-                  height={100}
-                  className="w-full"
+                  width={600}
+                  height={600}
+                  className="w-full md:max-w-[400px]"
                 />
               </div>
             ))}
           </div>
-        </motion.div>
+        </div>
 
         <section>
           <div className="grid grid-cols-2 gap-4 px-4 py-12 md:grid-cols-12">
@@ -98,7 +51,7 @@ export default function Home() {
             </div>
 
             <div className="flex flex-col gap-6 col-span-full md:col-span-6">
-              <p className="leading-7 font-semibold">
+              <p className="leading-7 text-xl font-semibold">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, sunt.
               </p>
 
@@ -118,7 +71,7 @@ export default function Home() {
                 {experiences.map((experience, index) => (
                   <div key={index}>
                     <div className="space-y-1">
-                      <dt className="font-semibold text-base">{experience.title}</dt>
+                      <dt className="font-semibold">{experience.title}</dt>
                       <dd className="text-gray-500">{experience.date}</dd>
                     </div>
                     <p className="leading-7 mt-2">{experience.description}</p>
@@ -132,22 +85,24 @@ export default function Home() {
 
       <footer className="bg-dark-gray text-off-white">
         <div className="grid grid-cols-2 gap-4 px-4 py-12 md:grid-cols-12">
-          <div className="col-span-full md:col-span-6 flex flex-col gap-12">
-            <h2 className="mt-10 scroll-m-20 text-6xl font-semibold text-off-white tracking-tight transition-colors first:mt-0">
-              Contato
-            </h2>
-
-            <dl className="[&>div]:mt-4 [&>div:first-child]:mt-0">
-              {contacts.map((contact, index) => (
-                <div key={index} className="space-y-1">
-                  <dt className="font-semibold text-[#6A6B71]">{contact.label}</dt>
-                  <dd className="text-off-white">
-                    {contact.value}
-                  </dd>
-                </div>
-              ))}
-            </dl>
-          </div>
+            <div className="col-span-full">
+              <h2 className="mb-8 scroll-m-20 text-6xl font-semibold text-off-white tracking-tight transition-colors">
+                Contato
+              </h2>
+            </div>
+              
+            <div className="col-span-full">
+              <dl className="flex flex-col md:flex-row gap-6 md:gap-8">
+                {contacts.map((contact, index) => (
+                  <div key={index} className="space-y-1">
+                    <dt className="font-semibold text-[#6A6B71]">{contact.label}</dt>
+                    <dd className="text-off-white">
+                      {contact.value}
+                    </dd>
+                  </div>
+                ))}
+              </dl>
+            </div>
         </div>
       </footer>
     </>
