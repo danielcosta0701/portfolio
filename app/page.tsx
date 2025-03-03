e630380c51411220f1e64475969a55421ae78ed0
danielcosta0701 <contatodanielskt@gmail.com>
2025-03-02 23:12:52 -0300

    fix: images

--- app/page.tsx
+++ app/page.tsx
@@ -9,22 +9,22 @@ export default function Home() {
       <main className="bg-off-white">
         <section className="min-h-dvh flex">
           <div className="flex-1 grid grid-cols-2 grid-rows-[1fr] gap-4 px-4 py-8 md:grid-cols-12">
-            <div className="col-span-full md:col-span-6 flex md:justify-center row-span-1">
-              <Image
-                src="/images/triz-01.JPG"
-                alt="Triz - Imagem 01"
-                width={600}
-                height={600}
-                className="w-full md:max-w-[400px]"
-              />
-            </div>
-
-            <div className="col-span-full md:col-span-6 flex flex-col gap-4 md:justify-center md:order-1">
+            <div className="col-span-full md:col-span-6 flex flex-col gap-4 md:justify-center">
               <h1 className="scroll-m-20 text-6xl font-bold tracking-tight lg:text-6xl">
                 Haaron Santana
               </h1>
 
-              <p className="leading- font-semilbold text-2xl">Capturando momentos inesquecíveis com a lente de quem vê o mundo de uma forma única</p>
+              {/* <p className="leading- font-semilbold text-2xl">Capturando momentos inesquecíveis com a lente de quem vê o mundo de uma forma única</p> */}
+            </div>
+
+            <div className="col-span-full md:col-span-6 flex md:justify-center row-span-1">
+              <Image
+                src="/images/haaron.jpg"
+                alt="Haaron Santana"
+                width={600}
+                height={600}
+                className="w-full"
+              />
             </div>
           </div>
         </section>
@@ -66,11 +66,7 @@ export default function Home() {
                 Sou autodidata, aprendendo por meio de tutoriais e prática constante. Há quatro anos, me aprofundei no universo do audiovisual, passando por agências renomadas, onde refinei minha técnica e meu olhar criativo.
               </p>
 
-              <p className="leading-7">
-                Hoje, meu trabalho é a fusão de uma visão única e uma técnica apurada, com o objetivo de capturar momentos que revelam a beleza e a essência de cada história.
-              </p>
-
-              <div className="bg-dark-gray">
+              {/* <div className="bg-dark-gray">
                 <Image
                   src="/images/haaron.jpg"
                   alt="Haaron Santana"
@@ -78,7 +74,7 @@ export default function Home() {
                   height={600}
                   className="w-full"
                 />
-              </div>
+              </div> */}
 
               {/* <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
                 Experiência
