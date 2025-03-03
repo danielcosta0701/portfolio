a28b7006f618277e6d903017dba0ecd60b76d3d0
danielcosta0701 <contatodanielskt@gmail.com>
2025-03-02 22:56:25 -0300

    feat: add links, fix image

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import images from "@/data/images";
-// import experiences from "@/data/experiences";
 import contacts from "@/data/contacts";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -9,7 +9,15 @@ export default function Home() {
       <main className="bg-off-white">
         <section className="min-h-dvh flex">
           <div className="flex-1 grid grid-cols-2 grid-rows-[1fr] gap-4 px-4 py-8 md:grid-cols-12">
-            <div className="col-span-full flex md:justify-center row-span-1">
+            <div className="col-span-full md:col-span-6 flex flex-col gap-4 md:justify-center">
+              <h1 className="scroll-m-20 text-6xl font-bold tracking-tight lg:text-6xl">
+                Haaron Santana
+              </h1>
+
+              <p className="leading- font-semilbold text-2xl">Capturando momentos inesquecíveis com a lente de quem vê o mundo de uma forma única</p>
+            </div>
+
+            <div className="col-span-full md:col-span-6 flex md:justify-center row-span-1">
               <Image
                 src="/images/triz-01.JPG"
                 alt="Triz - Imagem 01"
@@ -17,16 +25,6 @@ export default function Home() {
                 height={600}
                 className="w-full md:max-w-[400px]"
               />
-            </div>
-
-            <div className="col-span-full md:col-span-6">
-              <h1 className="scroll-m-20 text-6xl font-bold tracking-tight lg:text-6xl">
-                Haaron Santana
-              </h1>
-            </div>
-
-            <div className="col-span-full md:col-span-3">
-              <p className="leading- font-semilbold text-2xl">Capturando momentos inesquecíveis com a lente de quem vê o mundo de uma forma única</p>
             </div>
           </div>
         </section>
@@ -72,13 +70,13 @@ export default function Home() {
                 Hoje, meu trabalho é a fusão de uma visão única e uma técnica apurada, com o objetivo de capturar momentos que revelam a beleza e a essência de cada história.
               </p>
 
-              <div className="bg-dark-gray h-[400px]">
+              <div className="bg-dark-gray">
                 <Image
                   src="/images/haaron.jpg"
                   alt="Haaron Santana"
                   width={600}
                   height={600}
-                  className="w-full h-auto object-fit"
+                  className="w-full"
                 />
               </div>
 
@@ -116,7 +114,15 @@ export default function Home() {
                   <div key={index} className="space-y-1">
                     <dt className="font-semibold text-[#6A6B71]">{contact.label}</dt>
                     <dd className="text-off-white">
-                      {contact.value}
+                      {contact.type === "email" ? (
+                        <Link href={`mailto:${contact.value}`}>{contact.value}</Link>
+                      ) : contact.type === "url" && contact.url ? (
+                        <Link href={contact.url} target="_blank" rel="noopener noreferrer">
+                          {contact.value}
+                        </Link>
+                      ) : (
+                        contact.value
+                      )}
                     </dd>
                   </div>
                 ))}
